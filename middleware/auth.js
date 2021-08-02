cd726769bb88bef1e05ffbc0e14bdb38e25d648d
subhan <subhan97ahmed@gmail.com>
2021-08-03 02:57:21 +0500

    added cors options in auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,9 @@ const config=require('config')
 //middleware function get request , and process and return response....
 module.exports=function(req,res,next){//next use for jump to the next middleware function
     const token=req.header('x-auth-token');
+    // temp
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if(!token) return res.status(401).send("Access deninde token not provided");
     
     try{
