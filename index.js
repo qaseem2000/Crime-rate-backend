818775e7b96485ba1fb08b610da31c880d81b8d6
subhan <subhan97ahmed@gmail.com>
2021-08-03 00:49:41 +0500

    added corsOpitions

--- index.js
+++ index.js
@@ -22,8 +22,15 @@ mongoose.connect(config.connectionstring,{
 
 
 app.use(express.json());
-app.options('*', cors()) ;
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
+// app.options('*', cors()) ;
+// app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/api/users',users);
 app.use('/api/auth',auth);
