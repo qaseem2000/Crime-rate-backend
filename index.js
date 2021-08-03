a21c8ee1cc911f868ff0d7db2b1b68cad191f580
subhan <subhan97ahmed@gmail.com>
2021-08-03 22:47:26 +0500

    fixed cors

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ mongoose.connect(config.connectionstring, {
 
 
 app.use(express.json());
-const corsOptions ={
-  origin:'*', 
-  // credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
+// const corsOptions ={
+//   origin:'*', 
+//   // credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
 
-// app.options('*', cors());
-app.use(cors(corsOptions));
+app.options('*', cors());
+app.use(cors());
 
 app.use('/api/users', users);
 app.use('/api/auth', auth);
